14e28d2db5f19574cab2e8803ce13b388af7eeac
Angelitaaa17 <angela.heredia.romero.al@iespoligonosur.org>
2026-05-18 20:39:17 +0200

    ejercicio 3

---
+++ ambito.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    $valor1 = 10;
+    $valor2 = 5;
+
+    function prueba() {
+            global $valor, $valor2;
+            $valor3 = $valor1 + valor2;
+            echo $valor3;
+    }
+</body>
+</html>
---
+++ predefinidas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    //Predefinidas
+
+    $numero = 33;
+
+    echo "Nombre del servidor " . $_SERVER['SERVER_NAME'];
+    echo "<br>";
+    echo "Software del servidor " . $_SERVER['SERVER_SOFTWARE'];
+    echo "<br>";
+    echo " Puerto del servidor " . $_SERVER['SERVER_PORT'];
+    echo "<br>"
+
+    echo "La variable numero es " . $GLOBAL['numero'];
+</body>
+</html>
