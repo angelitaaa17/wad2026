76f75f5246b938c946bb4d9ff2e95d96fff2c3c7
Angelitaaa17 <angela.heredia.romero.al@iespoligonosur.org>
2026-05-18 21:28:26 +0200

    ejercicios 7

---
+++ operadores1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+        $num1 = 5;
+        $num2 = 2;
+
+        //Suma con operador aritmetico
+        $total = $num1 + $num2;
+        echo $total;
+        echo "<br>";
+        $resultado = $num2 % $num1;
+        echo $resultado;
+
+        echo "<br>";
+        $potencia = $num1 ** $num2;
+        echo $resultado;
+    ?>
+</body>
+</html>
---
+++ operadores2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+        $num1 = 5;
+        $num2 = 10;
+        $num3 = 0;
+
+        //Suma con operador aritmetico
+        $num3 = $num1;
+        echo $num3;
+        echo "<br>";
+        $num1+$num2; // $num1 = $num1 + $num2;
+        echo $num1;
+    ?>
+</body>
+</html>
