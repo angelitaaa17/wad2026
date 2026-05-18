f614a6684ca39e820a6b5b6fcd07af330933dbde
Angelitaaa17 <angela.heredia.romero.al@iespoligonosur.org>
2026-05-18 20:24:45 +0200

    ejercicios php 1

---
+++ plantilla.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    // Soy un comentario
+    /* Soy un comentario
+        de varias
+            ineas
+    */
+    # Soy un comentario de tipo Unix
+    <?
+        echo "Hola Mundo"
+    ?>
+</body>
+</html>
