33cd54d1e480589b393e55b2f601c1f867169c7e
Angelitaaa17 <angela.heredia.romero.al@iespoligonosur.org>
2026-05-18 21:06:54 +0200

    ejercicio 6

---
+++ arrays_sintaxis.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+        //Arrays
+        $dias = array("Lunes", "Martes", "Miercoles","Jueves","Viernes");
+
+        $numeros = array(10,20,30,40);
+
+        $nombres = array(10);
+
+        $datos = array();
+
+        $valores = [10,20,30,40];
+
+        echo $dias[3];
+        echo "<br>";
+
+        var_dump ($dias);
+
+        echo "<br>";
+
+        var_dump($dias);
+    ?>
+</body>
+</html>
