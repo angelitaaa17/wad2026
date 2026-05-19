b335357b093b4d9315e797e007cb20e5bcc2f6f0
Angelitaaa17 <angela.heredia.romero.al@iespoligonosur.org>
2026-05-19 21:19:58 +0200

    ejercicio 11

---
+++ Curso 2/procesar.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+
+<body>
+<?php
+ 
+    if(isset($_POST['usuario'])) {
+		if(!empty($_POST['usuario'])){
+			$usuario = $_POST['usuario'];
+			echo "Nombre de usuario: " . $usuario;
+			echo "<p> </p>";
+		}
+    }
+	 if(isset($_POST['password']) && !empty($_POST['password'])) {
+		$pass = $_POST['password'];
+		echo "Contraseña de usuario: " . $pass;
+		echo "<p> </p>";
+    }
+
+?>
+</body>
+</html>
