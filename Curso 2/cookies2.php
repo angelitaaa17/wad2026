0452593920857d1fe8fdcba44955e98dd0209a34
Angelitaaa17 <angela.heredia.romero.al@iespoligonosur.org>
2026-05-19 20:56:59 +0200

    ejercicio 8

---
+++ Curso 2/cookies2.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_GET['aceptar'])) {
+    $caducidad = time() + (60 * 60 * 24 * 365);
+    setcookie('micookie', '1', $caducidad);
+}
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+    <div>
+        <?php if (!isset($_GET['aceptar']) && !isset($_COOKIE['micookie'])): ?>
+            <div class="cookies">
+                <h2>Cookies</h2>
+                <p>Debes aceptar las cookies para poder seguir navegando</p>
+                <a href="?aceptar=1">Aceptar</a>
+            </div>
+        <?php endif; ?>
+    </div>
+</body>
+</html>
