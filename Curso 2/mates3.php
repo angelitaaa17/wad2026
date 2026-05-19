300095e0e7bbd87cdd25d584a6ff31d73ddfa425
Angelitaaa17 <angela.heredia.romero.al@iespoligonosur.org>
2026-05-19 20:36:44 +0200

    ejercicio 6

---
+++ Curso 2/mates3.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+
+<body>
+<?php
+
+	echo(abs(-6.7) . "<br/>");
+	
+	echo(max(2,4,6,8,10) . "<br/>");
+	
+	echo(min(2,4,6,8,10) . "<br/>");
+	
+	echo "<br>";
+	
+	echo(rand() . "<br/>");
+	echo(rand(10,100). "<br/>");
+	
+	echo "<br>";
+	
+	echo(sqrt(9) . "<br/>");
+	
+	echo "<br>";
+	
+	echo decbin("3") . "<br/>";
+	echo decbin("1587") . "<br/>";
+	echo dechex("10") . "<br/>";
+	echo bindec("1011") . "<br/>";
+	
+?>
+</body>
+</html>
